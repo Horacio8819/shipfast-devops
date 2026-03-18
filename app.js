ff917aa009bcac4e23eb9b62244b6a66f79d5f87
Horace Djousse Fofe <148912683+Horacio8819@users.noreply.github.com>
2026-03-18 14:57:05 +0100

    Update response message for root endpoint

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); 
 
 app.get('/', (req, res) => { 
-	res.send('ShipFast App is running ✅'); 
+	res.send('ShipFast deployed via CI/CD ✅'); 
 }); 
 
 app.get('/health', (req, res) => { 
