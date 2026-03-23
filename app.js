c8aa71373f923f2ab92223784c5097e726574043
Horace Djousse Fofe <148912683+Horacio8819@users.noreply.github.com>
2026-03-23 18:23:43 +0100

    Update deployment message in root route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); 
 
 app.get('/', (req, res) => { 
-	res.send('ShipFast deployed via CI/CD SSM  ✅'); 
+	res.send('ShipFast deployed via CI/CD SSM  second round ✅'); 
 }); 
 
 app.get('/health', (req, res) => { 
