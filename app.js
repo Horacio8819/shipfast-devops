1e95d310cf3eb583534f4e58758d343f66c07dd0
Horace Djousse Fofe <148912683+Horacio8819@users.noreply.github.com>
2026-04-15 18:10:23 +0200

    Update deployment message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); 
 
 app.get('/', (req, res) => { 
-	res.send('ShipFast deployed via CI/CD SSM  Horacio F✅'); 
+	res.send('ShipFast deployed via CI/CD SSM  Horacio ✅'); 
 }); 
 
 app.get('/health', (req, res) => { 
